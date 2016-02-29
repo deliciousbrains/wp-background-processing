ff5f15a7e2df2c3f1326ee029234eca18b214f0b
Ashley Rich <hello@ashleyrich.com>
2016-02-29 16:15:14 +0000

    Install table CLI command

---
+++ classes/cli-command.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace WP_Background_Processing;
+
+use WP_CLI;
+use WP_CLI_Command;
+
+/**
+ * Manage queue.
+ *
+ * @package wp-cli
+ */
+class CLI_Command extends WP_CLI_Command {
+
+	/**
+	 * Creates the queue table.
+	 *
+	 * @subcommand create-table
+	 */
+	public function create_table( $args, $assoc_args = array() ) {
+		global $wpdb;
+
+		$wpdb->hide_errors();
+
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE {$wpdb->prefix}queue (
+				id bigint(20) NOT NULL AUTO_INCREMENT,
+                action varchar(255) NOT NULL,
+                data longtext NOT NULL,
+                locked tinyint(1) NOT NULL DEFAULT 0,
+                created_at datetime NOT NULL,
+                PRIMARY KEY  (id)
+				) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+		dbDelta( $sql );
+
+		WP_CLI::success( "Table {$wpdb->prefix}queue created." );
+	}
+
+}
--- wp-background-processing.php
+++ wp-background-processing.php
@@ -10,3 +10,9 @@ Author URI: https://deliciousbrains.com/
 
 require_once plugin_dir_path( __FILE__ ) . 'classes/wp-async-request.php';
 require_once plugin_dir_path( __FILE__ ) . 'classes/wp-background-process.php';
+
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	require_once plugin_dir_path( __FILE__ ) . 'classes/cli-command.php';
+
+	WP_CLI::add_command( 'queue', 'WP_Background_Processing\CLI_Command' );
+}
