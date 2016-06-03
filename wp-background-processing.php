db6ba146cafbddc13e291832c57830ac9c2c3427
Brandon Hubbard <bhubbard@users.noreply.github.com>
2016-06-02 17:56:58 -0700

    Fixes #3

--- wp-background-processing.php
+++ wp-background-processing.php
@@ -12,6 +12,8 @@ Description: Asynchronous requests and background processing in WordPress.
 Author: Delicious Brains Inc.
 Version: 1.0
 Author URI: https://deliciousbrains.com/
+GitHub Plugin URI: https://github.com/A5hleyRich/wp-background-processing
+GitHub Branch: master
 */
 
 require_once plugin_dir_path( __FILE__ ) . 'classes/wp-async-request.php';
