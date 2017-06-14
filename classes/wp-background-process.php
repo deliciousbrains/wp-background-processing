03d17c5852e8d2cbb2209e264c366d5546dccfb7
Mike Jolley <mike.jolley@me.com>
2017-06-14 18:18:25 +0100

    Compare intval, since -1 can sometimes be a string
    Signed-off-by: Santiago Garza <santiago.garza@imforza.com>

--- classes/wp-background-process.php
+++ classes/wp-background-process.php
@@ -367,7 +367,7 @@ if ( ! class_exists( 'WP_Background_Process' ) ) {
 				$memory_limit = '128M';
 			}
 
-			if ( ! $memory_limit || -1 === $memory_limit ) {
+			if ( ! $memory_limit || -1 === intval( $memory_limit ) ) {
 				// Unlimited, set to 32GB.
 				$memory_limit = '32000M';
 			}
