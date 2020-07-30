1ad6f269f6e2c760e6857f157391c0c6c1ef447d
Iain <iain@polevaultweb.com>
2020-07-30 18:22:32 +0100

    Move the checking if classes exists to the plugin bootstrap file, instead of in the class files themselves.

--- wp-background-processing.php
+++ wp-background-processing.php
@@ -16,5 +16,9 @@ GitHub Plugin URI: https://github.com/A5hleyRich/wp-background-processing
 GitHub Branch: master
 */
 
-require_once plugin_dir_path( __FILE__ ) . 'classes/wp-async-request.php';
-require_once plugin_dir_path( __FILE__ ) . 'classes/wp-background-process.php';
+if ( ! class_exists( 'WP_Async_Request' ) ) {
+	require_once plugin_dir_path( __FILE__ ) . 'classes/wp-async-request.php';
+}
+if ( ! class_exists( 'WP_Background_Process' ) ) {
+	require_once plugin_dir_path( __FILE__ ) . 'classes/wp-background-process.php';
+}
