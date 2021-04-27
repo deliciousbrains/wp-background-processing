bb17964b080452e7369f0c58043a205466f6abf4
Tyler <tporadun@jettyapp.com>
2021-04-27 16:13:09 -0700

    refactor: create async exception

---
+++ src/BackgroundProcess/Exception/AsyncException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Jetty\BackgroundProcessing\BackgroundProcess\Exception;
+
+/**
+ * Exception thrown when an error occurs during an AsyncRequest.
+ */
+class AsyncException extends \RuntimeException {}
