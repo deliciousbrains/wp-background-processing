74f2040c64ba5a6a3e0740dc972bde204ecb4aed
Tyler <tporadun@jettyapp.com>
2021-05-03 13:33:40 -0700

    refactor: create batch item implementation for WP

---
+++ src/BackgroundProcess/Adapter/Out/Wp/WpBatchItem.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace Jetty\BackgroundProcessing\BackgroundProcess\Adapter\Out\Wp;
+
+use Jetty\BackgroundProcessing\BackgroundProcess\Domain\BatchItem;
+
+/**
+ * BatchItem implementation for WordPress meta tables.
+ */
+class WpBatchItem implements BatchItem
+{
+    /**
+     * @var string
+     */
+    private $key;
+
+    /**
+     * @var mixed
+     */
+    private $value;
+
+
+    public function __construct(string $key, $value)
+    {
+        $this->key = $key;
+        $this->value = $value;
+    }
+
+
+    /**
+     * The key (meta) of the batch item.
+     *
+     * @return string
+     */
+    public function key(): string
+    {
+        return $this->key;
+    }
+
+
+    /**
+     * The value of the item to be processed.
+     *
+     * @return mixed
+     */
+    public function value()
+    {
+        return $this->value;
+    }
+}
