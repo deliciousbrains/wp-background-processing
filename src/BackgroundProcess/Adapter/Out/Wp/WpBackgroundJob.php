83a6c647d32d34152050cd48a2239fe497639b0b
Tyler <tporadun@jettyapp.com>
2021-05-05 15:31:11 -0700

    refactor: add interface for background job

--- src/BackgroundProcess/Adapter/Out/Wp/WpBackgroundJob.php
+++ src/BackgroundProcess/Adapter/Out/Wp/WpBackgroundJob.php
@@ -3,22 +3,28 @@ declare(strict_types=1);
 
 namespace Jetty\BackgroundProcessing\BackgroundProcess\Adapter\Out\Wp;
 
-use Jetty\BackgroundProcessing\BackgroundProcess\Application\Port\Out\AsyncRequest;
+use Jetty\BackgroundProcessing\BackgroundProcess\Application\Port\Out\BackgroundJob;
 
 /**
  * Implements a background job for use with WordPress. Override handle() to
  * process the request.
  */
-abstract class WpBackgroundJob extends WpAjaxHandler implements AsyncRequest
+abstract class WpBackgroundJob extends WpAjaxHandler implements BackgroundJob
 {
     /**
      * @var string
      */
     private $actionName;
 
+    /**
+     * @var array
+     */
+    private $jobData;
+
     public function __construct(string $actionName)
     {
         $this->actionName = $actionName;
+        $this->jobData = [];
 
         parent::__construct($actionName);
     }
@@ -26,7 +32,15 @@ abstract class WpBackgroundJob extends WpAjaxHandler implements AsyncRequest
 
     final public function dispatch(array $data = []): array
     {
+        $requestData = array_merge($data, $this->jobData);
+
         $request = new WpAjaxRequest($this->actionName);
-        return $request->dispatch($data);
+        return $request->dispatch($requestData);
+    }
+
+
+    final public function setJobData(array $data): void
+    {
+        $this->jobData = $data;
     }
 }
