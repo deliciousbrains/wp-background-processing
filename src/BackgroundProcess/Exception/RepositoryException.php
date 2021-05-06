9ef3b3bf6cbee08028d16b5b9a7bbe4dc8b68a33
Tyler <tporadun@jettyapp.com>
2021-05-06 15:36:56 -0700

    refactor: add repository exception

---
+++ src/BackgroundProcess/Exception/RepositoryException.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Jetty\BackgroundProcessing\BackgroundProcess\Exception;
+
+use RuntimeException;
+
+/**
+ * Exception thrown when a repository operation fails.
+ */
+class RepositoryException extends RuntimeException
+{
+
+}
