bf3fbc73396cb03ba4b92b414f1d53471a443be0
Ian M. Jones <ian@ianmjones.com>
2023-04-12 16:49:14 +0100

    Add skeleton unit tests setup

---
+++ tests/test-it-works.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class TestItWorks
+ *
+ * @package WP-Background-Processing
+ */
+
+/**
+ * Sample test case.
+ */
+class TestItWorks extends WP_UnitTestCase {
+
+	/**
+	 * Are unit tests set up correctly?
+	 */
+	public function test_it_works() {
+		$this->assertTrue( true );
+	}
+}
